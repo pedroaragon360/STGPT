d679c1539cb26a9bbbf7a46dfda44fbb7e9c4c52
pedroaragon360 <pedroaragon360@gmail.com>
2023-11-14 23:46:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == 
                         for steps in reversed(run_steps.data):
                             if hasattr(steps.step_details, 'tool_calls'):
                                 st.text(steps.step_details.tool_calls[0].code_interpreter.input)
-                                st.divider()
+                                st.markdown('---')
                     #if steps.tools[0].type == 'code_interpreter':
                         # Handle text content
                         if hasattr(content_part, 'text') and content_part.text:
