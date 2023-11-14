f22aa617bce7107f4f872815010353fdee13de74
pedroaragon360 <pedroaragon360@gmail.com>
2023-11-14 20:35:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == 
             st.write(run_steps.data)
             for steps in reversed(run_steps.data):
                 if hasattr(steps.step_details, 'tool_calls'):
-                    st.write("Code interpreter: " + steps.step_details.tool_calls.code_interpreter)
+                    st.write("Code interpreter: " + steps.step_details.tool_calls[0].code_interpreter.input)
             #if steps.tools[0].type == 'code_interpreter':
 
     st.session_state.messages = client.beta.threads.messages.list(
