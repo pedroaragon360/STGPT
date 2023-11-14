9c4ff431f8ccef1232507936e17f56d94dbb2fff
pedroaragon360 <pedroaragon360@gmail.com>
2023-11-14 20:06:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,9 +99,9 @@ elif hasattr(st.session_state.run, 'status') and st.session_state.run.status == 
     )
     for iRun in reversed(st.session_state.runs.data):
         if iRun.tools[0].type == 'code_interpreter':
-            run = client.beta.threads.runs.retrieve(thread_id=st.session_state.thread.id,run_id=iRun.id)
+            run_steps = client.beta.threads.runs.steps.list(thread_id=st.session_state.thread.id,run_id=iRun.id  )
             with st.chat_message("assistant"):
-                st.write(run)
+                st.write(run_steps)
 
     st.session_state.messages = client.beta.threads.messages.list(
         thread_id=st.session_state.thread.id
