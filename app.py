a06f10994c32ec2430e4960ec3a4319af444beb7
pedroaragon360 <pedroaragon360@gmail.com>
2023-11-16 21:39:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,53 +27,14 @@ else:
     thread = client.beta.threads.retrieve(st.session_state["thread_id"])
 
 
-def ts_to_str(ts):
-    return datetime.datetime.fromtimestamp(ts).strftime("%Y-%m-%d %H:%M:%S")
-
-
-def gen_file_name(f):
-    return f"{f.filename} ({ts_to_str(f.created_at)}: {f.id})"
-
-
-select_file_tab, upload_file_tab = st.columns(2)
-
-with select_file_tab:
-    uploaded_files = client.files.list(purpose="assistants")
-    selected_files = st.multiselect(
-        "Select uploaded files", [gen_file_name(f) for f in uploaded_files]
-    )
-
-with upload_file_tab:
-    uploaded_file = st.file_uploader("Upload a file", type=["txt", "pdf", "docx", "md"])
-    if uploaded_file is not None:
-        st.info("File uploaded successfully, now parsing...")
-        file = client.files.create(file=uploaded_file, purpose="assistants")
-        st.info(
-            f"File parsed successfully, file id is {file.id}. now creating assistant..."
-        )
-
-
-if not uploaded_file and not selected_files:
-    st.error("Please upload a file or select a file to continue")
-    st.stop()
-file_ids = [f.id for f in uploaded_files if gen_file_name(f) in selected_files]
-if uploaded_file:
-    file_ids.append(file.id)
-print(selected_files, file_ids)
-
 
 def get_response(prompt: str):
-    client.beta.assistants.update(
-        assistant_id=assistant.id,
-        file_ids=file_ids,
-    )
     message = client.beta.threads.messages.create(
         thread.id, role="user", content=prompt
     )
     run = client.beta.threads.runs.create(
         thread_id=thread.id,
-        assistant_id=assistant.id,
-        instructions="Please address the user as Jane Doe. The user has a premium account.",
+        assistant_id=assistant.id
     )
 
     with st.spinner("Running assistant..."):
